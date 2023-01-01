14622777ebc9aa6249d015cbc79c79aeabc43b8a
KryukovSA <sergey.krykov.2806@gmail.com>
2023-01-01 16:17:16 +0300

    del directory

--- lab2/Requests/src/test/java/com/example/requests/RequestsApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.example.requests;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RequestsApplicationTests {
-
-}
