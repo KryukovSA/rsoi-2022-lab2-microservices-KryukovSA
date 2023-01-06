fe5915680752e6ab67e2f80624176bc710585213
KryukovSA <sergey.krykov.2806@gmail.com>
2023-01-06 14:21:40 +0300

    what db?

--- init.sql
+++ init.sql
@@ -1,3 +1,23 @@
+CREATE TABLE books
+(
+    id        SERIAL PRIMARY KEY,
+    book_uid  uuid UNIQUE  NOT NULL,
+    name      VARCHAR(255) NOT NULL,
+    author    VARCHAR(255),
+    genre     VARCHAR(255),
+    condition VARCHAR(20) DEFAULT 'EXCELLENT'
+        CHECK (condition IN ('EXCELLENT', 'GOOD', 'BAD'))
+);
+
+CREATE TABLE library
+(
+      id          SERIAL PRIMARY KEY,
+      library_uid uuid UNIQUE  NOT NULL,
+      name        VARCHAR(80)  NOT NULL,
+      city        VARCHAR(255) NOT NULL,
+      address     VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE reservation
 (
     id              SERIAL PRIMARY KEY,
@@ -10,26 +30,6 @@ CREATE TABLE reservation
     start_date      TIMESTAMP   NOT NULL,
     till_date       TIMESTAMP   NOT NULL
 )
-
-CREATE TABLE library
-(
-    id          SERIAL PRIMARY KEY,
-    library_uid uuid UNIQUE  NOT NULL,
-    name        VARCHAR(80)  NOT NULL,
-    city        VARCHAR(255) NOT NULL,
-    address     VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE books
-(
-    id        SERIAL PRIMARY KEY,
-    book_uid  uuid UNIQUE  NOT NULL,
-    name      VARCHAR(255) NOT NULL,
-    author    VARCHAR(255),
-    genre     VARCHAR(255),
-    condition VARCHAR(20) DEFAULT 'EXCELLENT'
-        CHECK (condition IN ('EXCELLENT', 'GOOD', 'BAD'))
-);
 
 CREATE TABLE library_books
 (
